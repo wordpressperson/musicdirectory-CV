a1ecfbd6772e959620712882e20a853c6ceb8dba
Ayokunle <therealayokunle@gmail.com>
2023-08-12 03:37:25 +0100

    oif

---
+++ build/precache-manifest.a6a2c4a6f3ef5d2bb50902c76d19d902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1208e5f105f92d06ff1",
+    "url": "/static/css/main.28df7890.chunk.css"
+  },
+  {
+    "revision": "a1208e5f105f92d06ff1",
+    "url": "/static/js/main.a1208e5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "db921c975cc05de7cda8",
+    "url": "/static/js/2.db921c97.chunk.js"
+  },
+  {
+    "revision": "8a04f63f44292f1dcc61ed6ab26a0f38",
+    "url": "/index.html"
+  }
+];
